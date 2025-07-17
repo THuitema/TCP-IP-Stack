e5e216e0220eb61891813938f151c1483bbfa415
Thomas Huitema <thuitema35@gmail.com>
2025-07-16 22:20:48 -0400

    Organize packet capturing process, implement ICMP processing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@
 mod ethernet;
 mod ip;
 mod icmp;
-use ethernet::{capture_ethernet_frames};
 use ip::IPv4Packet;
-use pcap::{Capture, Device};
+use pcap::{Active, Capture, Device};
+
+use crate::{ethernet::EthernetFrame, icmp::ICMPPacket};
 
 fn main() {
     let device = Device::lookup().unwrap().unwrap();
@@ -15,23 +16,50 @@ fn main() {
     }
 
     let mut cap: Capture<pcap::Active> = device.open().expect("Failed to open device");
-    let ethernet_frames = capture_ethernet_frames(&mut cap, 2);
+    capture_loop(&mut cap, 1000);
+}
 
-    for frame in ethernet_frames {
-        println!("{}", frame);
-        match frame.header.ethertype_to_protocol_name().as_str() {
-            "IPv4" => match IPv4Packet::from_bytes(&frame.payload) {
-                Ok(ip_packet) => {
-                    println!("{}", ip_packet)
+fn capture_loop(capture: &mut Capture<Active>, size: usize) {
+    let mut count = 0;
+
+    while let Ok(packet) = capture.next_packet() {
+        count += 1;
+        match EthernetFrame::from_bytes(packet.data) {
+            Ok(ethernet_frame) => match ethernet_frame.header.ethertype_to_protocol_name().as_str() {
+                "IPv4" => handle_ip_packet(&ethernet_frame),
+                s => {
+                    println!("{} packets not yet supported", s);
                 }
-                Err(e) => eprintln!("{}", e),
-            },
-            s => {
-                println!("{} packets not yet supported", s);
-            }
-        };
+            },    
+            Err(e) => eprintln!("{}", e),
+        }
+
+        if count > size {
+            break;
+        }
     }
 }
+
+fn handle_ip_packet(frame: &EthernetFrame) {
+    match IPv4Packet::from_bytes(&frame.payload) {
+        Ok(ip_packet) => match ip_packet.header.get_protocol_name().as_str() {
+            "ICMP" => {
+                println!("{}", ip_packet);
+                handle_icmp_packet(&ip_packet);
+            },
+            _ => ()//println!("{} packet read", s)
+        }
+        Err(e) => eprintln!("{}", e),
+    }
+}
+
+fn handle_icmp_packet(packet: &IPv4Packet) {
+    match ICMPPacket::from_bytes(&packet.payload) {
+        Ok(icmp_packet) => println!("{}", icmp_packet),
+        Err(e) => eprintln!("{}", e),
+    }
+}
+
 
 fn get_devices() {
     let devices = Device::list().unwrap();
