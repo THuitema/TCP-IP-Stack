cb7e3f605acb69ee6a3751bd89297a2fb8ad88a5
Thomas Huitema <thuitema35@gmail.com>
2025-08-15 01:26:34 -0400

    Update example in main() to remove hardcoded MAC address for router

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,23 @@
 #![allow(dead_code)]
 
-use tcpip_stack::ethernet::MACAddress;
-use tcpip_stack::ip::IPv4Address;
 use tcpip_stack::parse::{ParsedPacket, Transport};
 use tcpip_stack::utils::{ping, test_udp_send};
 use tcpip_stack::icmp::process_icmp;
 use tcpip_stack::addr_info::{AddrInfo, setup_addr_info};
-use tcpip_stack::udp::{process_udp, UDPSocket};
+use tcpip_stack::udp::{process_udp};
 
 fn main() {
-    // Need to hardcode MAC address of router until we implement ARP
-    let router_mac = MACAddress::new(0xc8, 0xa7, 0xa, 0x90, 0x9, 0x48);
-
-    let addr_info: AddrInfo = match setup_addr_info(Some("en0"), 54456, router_mac) {
+    let mut addr_info: AddrInfo = match setup_addr_info(Some("en0"), 2048) {
         Ok(s) => s,
         Err(e) => {
             eprintln!("{}", e);
             return;
         }
     };
-    // capture_loop(&mut addr_info, 100);
-    // test_udp_send(&mut addr_info);
 
-    let sock = UDPSocket::bind(addr_info).unwrap();
-    
-    for _ in 0..10 {
-        let packet = sock.recv().unwrap();
-        if let Transport::UDP(datagram) = &packet.transport {
-            println!("{} (port {})", packet, datagram.dest_port());
-            println!("{}", datagram);
-        }
-        
-    }
+    println!("{} {} {}", addr_info.addr_ipv4, addr_info.addr_mac, addr_info.router_mac);
+    // capture_loop(&mut addr_info, 100);
+    test_udp_send(&mut addr_info);
 }
 
 fn capture_loop(addr_info: &mut AddrInfo, size: usize) {
